924672078d86267f2fb5b0ed2584bdf1b6f13742
Roberto Cerezuela <roberto.cerezuela@oneclick.es>
2025-03-24 13:32:47 +0100

    updated build script dev

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,4 @@
 set -e
 
 export DOCKER_TAG=dev
-docker build -t up2university/learninglocker2-app:$DOCKER_TAG app
-docker build -t up2university/learninglocker2-nginx:$DOCKER_TAG nginx
-
+docker build --progress=plain -t up2university/learninglocker2-app:$DOCKER_TAG app
